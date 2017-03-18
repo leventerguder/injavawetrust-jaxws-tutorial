604eab398dddc212cb65a357a91be3b2dc33e6b9
leventerguder <erguder.levent@gmail.com>
2017-03-18 23:42:13 +0300

    jaxws soap

---
+++ injavawetrust.jaxws.trace/src/client/TracePropertyClient.java
@@ -0,0 +1,33 @@
+package client;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+
+import service.TraceWriter;
+
+public class TracePropertyClient {
+
+	// TCP/IP Monitor "local port"
+	private static String WEB_SERVICE_WSDL_URL = "http://localhost:8082/injavawetrust/trace.writer?wsdl";
+
+	public static void main(String[] args) throws MalformedURLException {
+
+		// When you invoke the Web Service, just pass the system property
+		// com.sun.xml.ws.transport.http.client.HttpTransportPipe.dump=true, it
+		// prints out the request and response message.
+
+		System.setProperty("com.sun.xml.internal.ws.transport.http.client.HttpTransportPipe.dump", "true");
+
+		URL url = new URL(WEB_SERVICE_WSDL_URL);
+		QName qname = new QName("http://service/", "TraceWriterImplService");
+		Service service = Service.create(url, qname);
+
+		TraceWriter traceWriter = service.getPort(TraceWriter.class);
+
+		String message = traceWriter.getMessage();
+		System.out.println(message);
+	}
+}
