5c57fd4b07d5051095428a774f040f3db2d91b0c
leventerguder <erguder.levent@gmail.com>
2017-10-27 23:57:17 +0300

    remove project

--- ISBNService/src/com/daehosting/webservices/isbn/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://webservices.daehosting.com/ISBN", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.daehosting.webservices.isbn;
